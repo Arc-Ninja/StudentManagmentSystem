199ced693f2c7b4a8fda7e46310122a93b248f17
avi9456 <gakcool9988@gmail.com>
2024-01-22 18:24:46 +0530

    add scroll bar

--- APP.java
+++ APP.java
@@ -13,22 +13,29 @@ public class APP {
     static public APPMenuBar search;
     static public entryPanel entry;
     static public result res;
+    static public JScrollPane scrollPane;
     static public ActionListener eventHandler;
     public static void main(String[] args) {
+
         eventHandler = new EventHandle(); 
         frame = new JFrame("TestFrame");
         frame.setLayout(new BorderLayout());
         viewJPanel = new JPanel();
         viewJPanel.setBackground(new Color(54, 54, 54));
         viewJPanel.setLayout(new FlowLayout(FlowLayout.CENTER,10,20));
+        
+        
         search = new APPMenuBar();
         entry = new entryPanel();
         res= new result();
         search.searchButton.addActionListener(eventHandler);
         search.entryButton.addActionListener(eventHandler);
         viewJPanel.add(entry);
+        scrollPane = new JScrollPane(viewJPanel,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        scrollPane.setViewportView(viewJPanel);
+        scrollPane.setPreferredSize(new Dimension(900,700));
         frame.add(search, BorderLayout.NORTH);
-        frame.add(viewJPanel, BorderLayout.CENTER);
+        frame.add(scrollPane, BorderLayout.CENTER);
         frame.setSize(1080,720);
         frame.setResizable(true);
         frame.setVisible(true);
