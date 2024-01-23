0ad56345d599157f9b72b17f6643fd04b05301e9
Arc <arcshaw99@gmail.com>
2024-01-23 23:25:00 +0530

    server

--- client.java
+++ client.java
@@ -3,14 +3,20 @@ import java.net.*;
 
 public class client {
     public static void main(String[] args) {
-        try{      
-    Socket s=new Socket("localhost",6666);  
-    DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
-    dout.writeUTF("Hello Server");  
-    dout.flush();  
-    dout.close();  
-    s.close();  
-    }
-    catch(Exception e){System.out.println(e);}  
+        try {
+            Socket s = new Socket("localhost", 6666);
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            dout.writeUTF("randi");
+            dout.writeUTF("continue");
+            for(int i=0;i<5;i++){
+            dout.writeUTF("data");
+            dout.writeUTF("continue");
+            }
+            dout.flush();
+            dout.close();
+            s.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
--- server.java
+++ server.java
@@ -1,15 +1,68 @@
 import java.net.*;
 import java.io.*;
 
-public class server {
-    public static void main(String[] args){  
-try{  
-ServerSocket ss=new ServerSocket(6666);  
-Socket s=ss.accept();//establishes connection   
-DataInputStream dis=new DataInputStream(s.getInputStream());  
-String  str=(String)dis.readUTF();  
-System.out.println("message= "+str);  
-ss.close();  
-}catch(Exception e){System.out.println(e);}  
-} 
+public class server extends Thread {
+    ServerSocket ss;
+    String exit = "exit";
+    DataInputStream dis;
+    Socket s;
+
+    public server() throws Exception {
+        ss = new ServerSocket(6666, );
+    }
+
+    public void connect() throws Exception {
+
+        try {
+
+            String connectivity = (String) dis.readUTF();
+            System.out.println(connectivity);
+            while (!connectivity.equals(exit)) {
+                String data = (String) dis.readUTF();
+                System.out.println(data);
+                connectivity = (String) dis.readUTF();
+            }
+            s.close();
+        } catch (Exception e) {
+        }
+
+    }
+
+    public void listen() throws Exception {
+        while (true) {
+            s = ss.accept();// establishes connection
+            dis = new DataInputStream(s.getInputStream());
+            String key = (String) dis.readUTF();
+            System.out.println(key);
+            if (!key.equals("randi")) {
+                s.close();
+            }
+        }
+    }
+
+    public void run() {
+        try {
+            this.connect();
+        } catch (Exception e) {
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        server s = new server();
+
+
+        Thread lis = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    s.listen();
+                } catch (Exception e) {
+                }
+            }
+        });
+        lis.start();
+
+        s.start();
+
+    }
+
 }
