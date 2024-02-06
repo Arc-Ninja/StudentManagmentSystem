512f4f560b2d3573f491e080e6bd908607791cc6
Arc <arcshaw99@gmail.com>
2024-02-06 20:42:09 +0530

    Client limit in server

--- Testing/Test/Client.java
+++ Testing/Test/Client.java
@@ -1,42 +1,48 @@
 package Testing.Test;
+
 import java.net.*;
 import java.io.*;
 import java.util.*;
 import stud.*;
 import stud.helpler.*;
+
 public class Client {
     Socket socket;
     DataInputStream in;
     DataOutputStream out;
     ObjectOutputStream oout;
-    public Client()throws Exception {
+
+    public Client() throws Exception {
         socket = new Socket("localhost", 6666);
-        in= new DataInputStream( socket.getInputStream() );
-        out= new DataOutputStream( socket.getOutputStream() );
-        oout= new ObjectOutputStream( socket.getOutputStream() );
-        System.out.println(socket.getLocalAddress()+":"+socket.getLocalPort()+":"+socket.getPort());
-        oout.writeObject(new Student(12345L, new Name("Avinash","Gupta"), new stud.helpler.Date(15,02,2002), "male"));
+        in = new DataInputStream(socket.getInputStream());
+        out = new DataOutputStream(socket.getOutputStream());
+        oout = new ObjectOutputStream(socket.getOutputStream());
+        System.out.println(socket.getLocalAddress() + ":" + socket.getLocalPort() + ":" + socket.getPort());
+        oout.writeObject(
+                new Student(12345L, new Name("Avinash", "Gupta"), new stud.helpler.Date(15, 02, 2002), "male"));
     }
+
     public void chat() throws Exception {
         Scanner cin = new Scanner(System.in);
-        String msg=cin.nextLine();;
-        
+        String msg = cin.nextLine();
+
         out.writeUTF(msg);
-        if(!msg.equals("exit")){
-            msg=(String)in.readUTF();
+        if (!msg.equals("exit")) {
+            msg = (String) in.readUTF();
             System.out.println(msg);
-            while(!msg.equals("exit")){
-                msg=cin.nextLine();
+            while (!msg.equals("exit")) {
+                msg = cin.nextLine();
                 out.flush();
                 out.writeUTF(msg);
             }
         }
 
-    } 
+    }
+
     public static void main(String[] args) throws Exception {
         Client client = new Client();
         client.chat();
         client.socket.close();
     }
-    
+
 }
--- Testing/Test/Server.java
+++ Testing/Test/Server.java
@@ -1,72 +1,83 @@
 package Testing.Test;
+
 import java.net.*;
 import java.io.*;
 import stud.*;
 import stud.helpler.*;
-class ClientHandler extends Thread{
+
+class global{
+    protected static int i=0;
+}
+
+
+class ClientHandler extends Thread {
     Socket socket;
     DataInputStream in;
     DataOutputStream out;
     ObjectInputStream oin;
-    public ClientHandler(Socket socket,int i) throws Exception{
+
+    public ClientHandler(Socket socket, int i) throws Exception {
         super(String.valueOf(i));
         this.socket = socket;
         this.socket.setKeepAlive(true);
-        socket=null;
+        socket = null;
         in = new DataInputStream(this.socket.getInputStream());
         out = new DataOutputStream(this.socket.getOutputStream());
         oin = new ObjectInputStream(this.socket.getInputStream());
-        Student std= (Student)oin.readObject();
+        Student std = (Student) oin.readObject();
         std.showDetails();
     }
-    public void run(){
-        try{
-        String msg=(String)in.readUTF();
-        if(msg.equals("exit")){
-            socket.close();
-        }
-        else{
-            out.writeUTF(socket.getLocalAddress()+":"+socket.getLocalPort());
-            do{
-                System.out.println(socket.getInetAddress()+":"+socket.getPort()+"->"+msg);
-                msg=(String)in.readUTF();
-            }while(!msg.equals("exit"));
-            // socket.close();
-        }
-        }catch(Exception e){
 
-        }finally{
-            System.out.println(getName()+": completed!");
+    public void run() {
+        try {
+            String msg = (String) in.readUTF();
+            if (msg.equals("exit")) {
+                socket.close();
+                global.i--;
+            } else {
+                out.writeUTF(socket.getLocalAddress() + ":" + socket.getLocalPort());
+                do {
+                    System.out.println(socket.getInetAddress() + ":" + socket.getPort() + "->" + msg);
+                    msg = (String) in.readUTF();
+                } while (!msg.equals("exit"));
+                // socket.close();
+            }
+        } catch (Exception e) {
+
+        } finally {
+            System.out.println(getName() + ": completed!");
         }
 
     }
 }
-public class Server{
+
+public class Server {
     public ServerSocket socket;
-    public Server()throws Exception{
+
+    public Server() throws Exception {
         socket = new ServerSocket(6666);
     }
-    public static void main(String[] arg)throws Exception{
+
+    public static void main(String[] arg) throws Exception {
+        // global numth = new global();
         Thread[] threads = new Thread[10];
         Server server = new Server();
-        int i=0;
-        while(i<3){
-            Socket socket = server.socket.accept();
-            threads[i]= new ClientHandler(socket,100+i);
-            // threads[i].
-            threads[i].start();
-            socket=null;
-            i++;
-        }
-        i=0;
-        while(i<3){
-            threads[i].join();
-            i++;
-        }
+        while (true) {
+            
+            if (global.i <= 10) {
+                Socket socket = server.socket.accept();
+                threads[global.i] = new ClientHandler(socket, 100 + global.i);
+                // threads[i].
+                threads[global.i].start();
+                socket = null;
+                global.i++;
+            }
+            
 
 
+
+        }
 
     }
-
 
 }
