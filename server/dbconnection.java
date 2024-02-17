7a3a1dcacd671089ee6e550d0a33d6ef725a861b
Arc-Ninja <arcshaw99@gmail.com>
2024-02-17 16:01:53 +0530

    database connectivity fixed (gamma)

--- server/dbconnection.java
+++ server/dbconnection.java
@@ -1,17 +1,17 @@
 package server;
 import java.sql.*;
-import com.mysql.jdbc.Driver;
+
 public class dbconnection {
     public static void main(String[] args) {
         try{
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             String username = "root";
             String password = "";
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/student",username,password);
             Statement st = conn.createStatement();
             ResultSet rs = st.executeQuery("desc info;");
             while(rs.next()){
-                System.out.println(rs.getString(1)+" "+rs.getString(2));
+                System.out.println(rs.getString(1));
             }
             st.close();
             conn.close();
