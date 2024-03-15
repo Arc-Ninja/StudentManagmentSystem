47d24ae7b9ceabff7bb9460d9d86230c46dd866e
avi9456 <gakcool9988@gmail.com>
2024-03-15 15:46:22 +0530

    solution 8

--- Java_Programs/main8.java
+++ Java_Programs/main8.java
@@ -1,5 +1,20 @@
 // package Java_Programs;
 
 public class main8 {
-    static final long long odd = 
+    static final int odd = 0x55555555;
+    static final int even =0xAAAAAAAA;
+    public static void main(String[] args) {
+        int num;
+        if(args.length==0){
+            java.util.Scanner in = new java.util.Scanner(System.in);
+            System.out.println("enter 32 bit integer value: ");
+            num = in.nextInt();
+        }else{
+            num = Integer.parseInt(args[0]);
+        }
+        int o = (num & odd)<<1;
+        int e = (num & even)>>1;
+        System.out.println((o|e));
+
+    }
 }
