bd3dda79d07572d0dd0c2fe6aa26944c64efe2d7
avi9456 <gakcool9988@gmail.com>
2024-03-18 13:03:11 +0530

    set_2 problem 3

---
+++ Java_Programs2/main3.java
@@ -0,0 +1,31 @@
+// package Java_Programs2;
+
+public class main3 {
+    public static void main(String[] args) {
+        System.out.print("enter row of half hollow pyramid: ");
+        int R;
+        if(args.length==0){
+            java.util.Scanner in = new java.util.Scanner(System.in);
+            R=in.nextInt();
+        }else{
+            R=Integer.parseInt(args[0]);
+        }
+        for(int i=1;i<R;i++){
+            for(int j=1;j<=R-i;j++){
+                System.out.print(" ");
+            }
+            for(int col=1;col<=i;col++){
+                if(col==1||col==i){
+                    System.out.print(col+" ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        for(int i=1;i<=R;i++){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+}
