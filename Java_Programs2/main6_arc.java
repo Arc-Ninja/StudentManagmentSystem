99123aaf59c10928384a444e173e51677f2c1946
Arc-Ninja <arcshaw99@gmail.com>
2024-03-18 15:17:02 +0530

    main6 in progress

---
+++ Java_Programs2/main6_arc.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class main6_arc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+        int []x = new int[n];
+        int []y = new int[n];
+        for(int i=0;i<n;i++){
+            x[i] = sc.nextInt();
+            y[i] = sc.nextInt(); 
+        }
+        
+
+        
+
+
+    }
+}
