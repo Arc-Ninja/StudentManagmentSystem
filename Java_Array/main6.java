e739d6fc0be41015c741c8279dedf5042a3670a3
Arc-Ninja <arcshaw99@gmail.com>
2024-03-18 15:50:05 +0530

    main6.java done

--- Java_Array/main6.java
+++ Java_Array/main6.java
@@ -6,18 +6,19 @@ import java.io.*;
 public class main6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n;
+        int n,min=0;
         n = sc.nextInt();
         int []x = new int[n];
         int []y = new int[n];
         for(int i=0;i<n;i++){
             x[i] = sc.nextInt();
-            y[i] = sc.nextInt(); 
+            y[i] = sc.nextInt();
+
         }
-        
-
-        
-
+        for(int i=0;i<n-1;i++){
+            min += Math.abs(x[i]-x[i+1]) + Math.abs(y[i]-y[i+1]) - 1; 
+        }
+        System.out.println(min);
 
     }
 }
