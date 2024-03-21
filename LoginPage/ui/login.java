92c5ea9153d4a86464dd9e6f6aafa1d30984f6c9
Arc-Ninja <arcshaw99@gmail.com>
2024-03-21 12:28:20 +0530

    login page

--- LoginPage/ui/login.java
+++ LoginPage/ui/login.java
@@ -1,30 +1,65 @@
 package LoginPage.ui;
 import java.awt.*;
 import java.util.concurrent.Flow;
+import java.util.jar.JarEntry;
 
 import javax.swing.*;
 public class login extends JPanel{
-    JLabel user,pass;
+    JLabel user,pass,signin;
     JTextField username;
     JPasswordField password;
     JPanel log_box;
+    JButton login_butt, signin_butt;
     public login(){
+
+
+
         this.setBackground(new Color(0,0,0));
         this.setPreferredSize(new Dimension(600,800));
         setLayout(null);
+
+
         log_box = new JPanel();
         // log_box.setPreferredSize(new Dimension(450,300));
         log_box.setBackground(new Color(255,255,255));
         log_box.setLayout(null);
-        log_box.setBounds(70,200,450,270);
+        log_box.setBounds(70,200,450,300);
+
+
+
         user = new JLabel("Username: ");
         pass = new JLabel("Password: ");
-        user.setBounds(10,10,200,50);
-        pass.setBounds(10,50,200,50);
+        signin = new JLabel("Already have an account?");
+        username = new JTextField();
+        password = new JPasswordField();
+        username.setBounds(150,30,200,30);
+        password.setBounds(150,90,200,30);
+        signin.setBounds(100,200,250,30);
+        user.setBounds(20,30,150,30);
+        pass.setBounds(20,90,150,30);
+        signin.setFont(new Font("Georgia", Font.PLAIN, 18));
         user.setFont(new Font("Georgia", Font.PLAIN, 20));
         pass.setFont(new Font("Georgia", Font.PLAIN, 20));
+
+
+        login_butt = new JButton("Log In");
+        signin_butt = new JButton("Sign In");
+        login_butt.setBounds(150,150,100,35);
+        signin_butt.setBounds(150,250,100,35);
+        login_butt.setFont(new Font("Georgia", Font.PLAIN, 15));
+        signin_butt.setFont(new Font("Georgia", Font.PLAIN, 15));
+
+
+        log_box.add(login_butt);
+        log_box.add(signin_butt);
+        log_box.add(username);
+        log_box.add(password);
         log_box.add(user);
         log_box.add(pass);
+        log_box.add(signin);
+
+
+
         add(log_box);
         log_box.setVisible(true);
         setVisible(true);  
