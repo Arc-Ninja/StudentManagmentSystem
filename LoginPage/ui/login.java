38990e34272ff6e2b310370ec2f02775bdfcd129
Arc-Ninja <arcshaw99@gmail.com>
2024-03-21 12:57:28 +0530

    login page colors update

--- LoginPage/ui/login.java
+++ LoginPage/ui/login.java
@@ -14,14 +14,14 @@ public class login extends JPanel{
 
 
 
-        this.setBackground(new Color(0,0,0));
+        this.setBackground(new Color(28, 28, 28));
         this.setPreferredSize(new Dimension(600,800));
         setLayout(null);
 
 
         log_box = new JPanel();
         // log_box.setPreferredSize(new Dimension(450,300));
-        log_box.setBackground(new Color(255,255,255));
+        log_box.setBackground(new Color(77, 77, 77));
         log_box.setLayout(null);
         log_box.setBounds(70,200,450,300);
 
@@ -30,20 +30,44 @@ public class login extends JPanel{
         user = new JLabel("Username: ");
         pass = new JLabel("Password: ");
         signin = new JLabel("Already have an account?");
-        username = new JTextField();
-        password = new JPasswordField();
-        username.setBounds(180,30,200,30);
-        password.setBounds(180,90,200,30);
+
         signin.setBounds(130,200,250,30);
         user.setBounds(50,30,150,30);
         pass.setBounds(50,90,150,30);
+
         signin.setFont(new Font("Georgia", Font.PLAIN, 18));
         user.setFont(new Font("Georgia", Font.PLAIN, 20));
         pass.setFont(new Font("Georgia", Font.PLAIN, 20));
 
+        user.setForeground(new Color(255,255,255));
+        pass.setForeground(new Color(255,255,255));
+        signin.setForeground(new Color(255,255,255));
+
+
+        username = new JTextField();
+        password = new JPasswordField();
+
+        username.setBounds(180,30,200,30);
+        password.setBounds(180,90,200,30);
+
+        username.setBackground(new Color(51, 51, 51));
+        password.setBackground(new Color(51, 51, 51));
+        username.setForeground(new Color(255,255,255));
+        password.setForeground(new Color(255,255,255));
+
 
         login_butt = new JButton("Log In");
         signin_butt = new JButton("Sign In");
+
+        login_butt.setFocusPainted(false);
+        signin_butt.setFocusPainted(false);
+
+        login_butt.setBackground(new Color(0,0,0));
+        signin_butt.setBackground(new Color(0,0,0));
+
+        login_butt.setForeground(new Color(255,255,255));
+        signin_butt.setForeground(new Color(255,255,255));
+
         login_butt.setBounds(180,150,100,35);
         signin_butt.setBounds(180,245,100,35);
         login_butt.setFont(new Font("Georgia", Font.PLAIN, 15));
